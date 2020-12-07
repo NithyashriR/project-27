00525fedc34a93c053d142d451672b23020fe690
NithyashriR <72071584+NithyashriR@users.noreply.github.com>
2020-12-07 21:42:41 +0530

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,23 @@
+class Bob {
+    constructor(x,y,r){
+        var options={
+            isStatic:false,
+            friction:0.4,
+            density:3,
+           restitution:1.2,
+        }
+        this.x=x;
+        this.y=y;
+        this.r=r;
+
+        this.body=Bodies.circle(this.x,this.y,this.r/2,options)
+        World.add(world,this.body);
+    }
+    display(){
+        
+        fill (255);
+
+        ellipseMode(CENTER);
+        ellipse(0,0,this.r,this.r)
+        
+}
